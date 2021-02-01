736a9eff476d247188e003484528831d256a082a
andormade <hngrhorace@gmail.com>
2021-02-01 23:52:47 +0000

    fixed signedLEB128 to able to encode negative numbers

--- src/encoding.ts
+++ src/encoding.ts
@@ -12,9 +12,14 @@ export const encodeString = (str: string) => [
 export const signedLEB128 = (n: number) => {
   const buffer = [];
   let more = true;
+  const isNegative = n < 0;
+  const bitCount = Math.ceil(Math.log2(Math.abs(n))) + 1;
   while (more) {
     let byte = n & 0x7f;
     n >>>= 7;
+    if (isNegative) {
+      n = n | -(1 << (bitCount - 8));
+    }
     if ((n === 0 && (byte & 0x40) === 0) || (n === -1 && (byte & 0x40) !== 0)) {
       more = false;
     } else {
